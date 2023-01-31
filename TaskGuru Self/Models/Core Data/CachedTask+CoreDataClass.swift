f723648957527289930748ee18942acdec175dd4
Joe Pham <rungxanh2901@gmail.com>
2023-01-30 19:43:08 -0500

    add core data version of TaskItem

---
+++ TaskGuru Self/Models/Core Data/CachedTask+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CachedTask+CoreDataClass.swift
+//  TaskGuru Self
+//
+//  Created by Joe Pham on 2023-01-30.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CachedTask)
+public class CachedTask: NSManagedObject {
+
+}
