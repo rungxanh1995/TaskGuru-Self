cf4fdc27b0be5dc4d99c518addbd1af82c0148e9
Joe Pham <rungxanh2901@gmail.com>
2023-02-06 08:37:21 -0500

    fix indentation in topmost app scene

--- TaskGuru Self/TaskGuru_SelfApp.swift
+++ TaskGuru Self/TaskGuru_SelfApp.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 @main
 struct TaskGuru_SelfApp: App {
-    var body: some Scene {
-        WindowGroup {
+	var body: some Scene {
+		WindowGroup {
 			TabView {
 				HomeView()
 					.tabItem {
@@ -24,6 +24,6 @@ struct TaskGuru_SelfApp: App {
 					}
 			}
 			.setUpColorTheme()
-        }
-    }
+		}
+	}
 }
