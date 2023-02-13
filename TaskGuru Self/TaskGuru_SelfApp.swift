73efe92cf729a6edc250f13c40f38457d10a7610
Joe Pham <rungxanh2901@gmail.com>
2023-02-13 07:59:40 -0500

    update accent color to mint, alert no longer shows default blue color

--- TaskGuru Self/TaskGuru_SelfApp.swift
+++ TaskGuru Self/TaskGuru_SelfApp.swift
@@ -5,13 +5,20 @@
 //  Created by Joe Pham on 2023-01-27.
 //
 
+import UIKit
 import SwiftUI
 
 // swiftlint:disable type_name
 @main
 struct TaskGuru_SelfApp: App {
+
 	private var homeVM: HomeViewModel = .init()
 	private var appState: AppState = .init()
+
+	init() {
+		UIView.appearance(whenContainedInInstancesOf: [UIAlertController.self])
+			.tintColor = UIColor(Color("AccentColor"))
+	}
 
 	var body: some Scene {
 		WindowGroup {
