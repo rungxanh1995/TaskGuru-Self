8d3909c17799f433e02dc3265977dfccb74e94a1
Joe Pham <rungxanh2901@gmail.com>
2023-02-14 09:44:05 -0500

    fade in out when showing onboard screen

--- TaskGuru Self/TaskGuru_SelfApp.swift
+++ TaskGuru Self/TaskGuru_SelfApp.swift
@@ -32,6 +32,7 @@ struct TaskGuru_SelfApp: App {
 		WindowGroup {
 			if isOnboarding {
 				OnboardContainerView()
+					.transition(.asymmetric(insertion: .opacity.animation(.default), removal: .opacity))
 					.setUpColorTheme()
 			} else {
 				TabView {
@@ -57,6 +58,7 @@ struct TaskGuru_SelfApp: App {
 				}
 				.environmentObject(homeVM)
 				.environmentObject(appState)
+				.transition(.asymmetric(insertion: .opacity.animation(.default), removal: .opacity))
 				.setUpColorTheme()
 			}
 		}
