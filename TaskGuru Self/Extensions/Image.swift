154cecd0ac7b0bd6ed365ff20c1cc168b36fcd19
Joe Pham <rungxanh2901@gmail.com>
2023-02-26 09:28:37 -0500

    add extension for large icon image

--- TaskGuru Self/Extensions/Image.swift
+++ TaskGuru Self/Extensions/Image.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 extension Image {
+	/// Frames image as a large icon size, could be used in Onboarding screen.
+	func asLargeIconSize() -> some View {
+		self
+			.resizable()
+			.scaledToFit()
+			.frame(width: 84, height: 84)
+			.clipShape(RoundedRectangle(cornerRadius: 22))
+	}
+
 	/// Frames image as a medium icon size for a row in App icon settings view.
 	func asIconSize() -> some View {
 		self
