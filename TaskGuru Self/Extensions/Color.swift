d398846a8fa98ed49d39894c25d0a27bc264a521
Joe Pham <rungxanh2901@gmail.com>
2023-02-27 07:09:58 -0500

    edit accent color hex values

--- TaskGuru Self/Extensions/Color.swift
+++ TaskGuru Self/Extensions/Color.swift
@@ -17,33 +17,33 @@ extension Color {
 		}
 
 		switch accentColor {
-		case .berry: return Color(hex: 0xE14459)
-		case .orange: return Color(hex: 0xE65C39)
+		case .berry: return Color(hex: 0xE04459)
+		case .orange: return Color(hex: 0xE55B39)
 		case .yellow: return Color(hex: 0xE8A53A)
-		case .green: return Color(hex: 0x5DC555)
-		case .clover: return Color(hex: 0x56BD9F)
-		case .blue: return Color(hex: 0x4CA7DB)
-		case .indigo: return Color(hex: 0x445FB9)
-		case .purple: return Color(hex: 0x6647D8)
+		case .green: return Color(hex: 0x5DC556)
+		case .clover: return Color(hex: 0x57BD9F)
+		case .blue: return Color(hex: 0x4CA7DA)
+		case .indigo: return Color(hex: 0x445FB8)
+		case .purple: return Color(hex: 0x6647D7)
 		}
 	}
 
 	/// The custom pink color specific to TaskGuru
-	static var appPink: Color {	Color(hex: 0xE14459) }
+	static var appPink: Color {	Color(hex: 0xE04459) }
 	/// The custom orange color specific to TaskGuru
-	static var appOrange: Color { Color(hex: 0xE65C39) }
+	static var appOrange: Color { Color(hex: 0xE55B39) }
 	/// The custom yellow color specific to TaskGuru
 	static var appYellow: Color { Color(hex: 0xE8A53A) }
 	/// The custom green color specific to TaskGuru
-	static var appGreen: Color { Color(hex: 0x5DC555) }
+	static var appGreen: Color { Color(hex: 0x5DC556) }
 	/// The custom teal color specific to TaskGuru
-	static var appTeal: Color { Color(hex: 0x56BD9F) }
+	static var appTeal: Color { Color(hex: 0x57BD9F) }
 	/// The custom blue color specific to TaskGuru
-	static var appBlue: Color { Color(hex: 0x4CA7DB) }
+	static var appBlue: Color { Color(hex: 0x4CA7DA) }
 	/// The custom indigo color specific to TaskGuru
-	static var appIndigo: Color { Color(hex: 0x445FB9) }
+	static var appIndigo: Color { Color(hex: 0x445FB8) }
 	/// The custom indigo color specific to TaskGuru
-	static var appPurple: Color { Color(hex: 0x6647D8) }
+	static var appPurple: Color { Color(hex: 0x6647D7) }
 
 	/// Allows initializing color from hex code with format "0xABCDEF"
 	init(hex: UInt, alpha: Double = 1) {
